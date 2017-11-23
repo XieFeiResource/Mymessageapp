04af013315a2ae6304a6d9b7b72f3b9cbecd1412
Administrator <Administrator@SC-201709302348>
2017-11-23 22:22:52 +0800

    add 11/23/10.20

--- messageApp/src/com/message/control/Server.java
+++ messageApp/src/com/message/control/Server.java
@@ -1,5 +1,30 @@
 package com.message.control;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+import com.message.serverconfig.SocketConfig;
+
+public class Server {
+	private ServerSocket server;
+	public Server() {
+		try {
+			server=new ServerSocket(SocketConfig.port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void StartService() {
+		try {
+			Socket client=server.accept();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		
+	}
 }
